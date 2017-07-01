b54353b4dd1bb898950d0f642c33e0aeb0e98d4f
Nikolay <Filirien@abv.bg>
2017-07-01 22:08:27 +0300

    Combining Data Structures - Lab and Exercise

---
+++ Exam Preparation/Problem-1-LimitedMemory/C#-Skeleton/LimitedMemory/LimitedMemoryCollection.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace LimitedMemory
+{
+    public class LimitedMemoryCollection<K, V> : ILimitedMemoryCollection<K, V>
+    {
+        private Dictionary<K, LinkedListNode<Pair<K, V>>> dic;
+        private LinkedList<Pair<K, V>> linkedList;
+        public LimitedMemoryCollection(int capacity)
+        {
+            this.Capacity = capacity;
+            this.Count = 0;
+            this.dic = new Dictionary<K, LinkedListNode<Pair<K, V>>>();
+            this.linkedList = new LinkedList<Pair<K, V>>();
+
+        }
+
+        public IEnumerator<Pair<K, V>> GetEnumerator()
+        {
+            var currentElement = linkedList.Last;
+            while (currentElement != null)
+            {
+                yield return currentElement.Value;
+            currentElement = currentElement.Previous;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public int Capacity { get; private set; }
+
+        public int Count { get; private set; }
+
+        public void Set(K key, V value)
+        {
+            if (!dic.ContainsKey(key))
+            {
+                var newNode = new LinkedListNode<Pair<K, V>>(new Pair<K, V>(key, value));
+                if (Count == Capacity)
+                {
+                    var firstElement = linkedList.First;
+
+                    linkedList.RemoveFirst();
+                    dic.Remove(firstElement.Value.Key);
+                    this.Count--;
+                }
+                linkedList.AddLast(newNode);
+                dic[key] = newNode;
+                Count++;
+            }
+            else
+            {
+                var elementToRemove = dic[key];
+                linkedList.Remove(elementToRemove);
+                elementToRemove.Value = new Pair<K, V>(key, value);
+                linkedList.AddLast(elementToRemove);
+            }
+        }
+
+        public V Get(K key)
+        {
+            if (!dic.ContainsKey(key))
+            {
+                throw new KeyNotFoundException();
+            }
+            var elementToRemove = dic[key];
+            linkedList.Remove(elementToRemove);
+            linkedList.AddLast(elementToRemove);
+            return elementToRemove.Value.Value;
+        }
+    }
+}
---
+++ Exam Preparation/Problem-2-Scoreboard/C# Skeleton/Scoreboard/Scoreboard/Scoreboard.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class Scoreboard : IScoreboard
+{
+    public int MaxEntriesToKeep { get; set; }
+    private Dictionary<string, string> registerUser;
+    private Dictionary<string, string> registerGame;
+    private Dictionary<string, OrderedBag<ScoreboardEntry>> scoreBoard;
+    public Scoreboard(int maxEntriesToKeep = 10)
+    {
+        this.registerUser = new Dictionary<string, string>();
+        this.registerGame = new Dictionary<string, string>();
+        this.scoreBoard = new Dictionary<string, OrderedBag<ScoreboardEntry>>();
+        this.MaxEntriesToKeep = maxEntriesToKeep;
+
+    }
+
+    public bool RegisterUser(string username, string password)
+    {
+        if (!registerUser.ContainsKey(username))
+        {
+            registerUser[username] = password;
+            return true;
+        }
+        return false;
+    }
+
+    public bool RegisterGame(string game, string password)
+    {
+        if (!registerGame.ContainsKey(game))
+        {
+            registerUser[game] = password;
+            scoreBoard[game] = new OrderedBag<ScoreboardEntry>();
+            return true;
+        }
+        return false;
+    }
+
+    public bool AddScore(string username, string userPassword, string game, string gamePassword, int score)
+    {
+        if (scoreBoard.ContainsKey(game) && registerGame[game] == gamePassword && registerUser.ContainsKey(username) && registerUser[username] == userPassword)
+        {
+            scoreBoard[game].Add(new ScoreboardEntry() { Username = username, Score = score});
+            return true;
+        }
+        return false;
+    }
+
+    public IEnumerable<ScoreboardEntry> ShowScoreboard(string game)
+    {
+        return scoreBoard[game].Take(MaxEntriesToKeep);
+    }
+
+    public bool DeleteGame(string game, string gamePassword)
+    {
+        if (registerGame.ContainsKey(game) && registerGame[game] != gamePassword) 
+        {
+            registerGame.Remove(game);
+            scoreBoard.Remove(game);
+            return true;
+        }
+        return false;
+    }
+
+    public IEnumerable<string> ListGamesByPrefix(string gameNamePrefix)
+    {
+        throw new NotImplementedException();
+    }
+}
